84eb0638538655ddcacef3716b44955aa5b5c51e
Maran2002 <110366816+Maran2002@users.noreply.github.com>
2024-09-11 21:57:15 +0530

    Add files via upload

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import NewsList from './NewsList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='header'>News App
+        <span> - Maran</span>
+      </h1>
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ Server/server.js
@@ -0,0 +1,49 @@
+// server.js
+const express = require("express");
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "https://maran-gnews.web.app",
+      "http://localhost:3000",
+      "https://maran-gnews.firebaseapp.com",
+    ], // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+const GNEWS_API_KEY = process.env.GNEWS_API_KEY;
+const GNEWS_BASE_URL = "https://gnews.io/api/v4";
+
+// Fetch news from gnews.io API
+app.get("/api/news", async (req, res) => {
+  const { query, lang, country, max, page = 1 } = req.query;
+  try {
+    const response = await fetch(
+      `${GNEWS_BASE_URL}/search?q=${query}&token=${GNEWS_API_KEY}&lang=${lang}&country=${country}&page=${page}&max=${max}`
+    );
+    const data = await response.json();
+
+    if (response.ok) {
+      res.json(data);
+    } else {
+      res.status(response.status).json({ error: data.message });
+    }
+    // res.json(sample)
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching news" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
